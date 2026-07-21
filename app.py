ea5771c10da94fb585e36769cb01c0f15c43d33f
Juan <jjuanleon.ti@gmail.com>
2026-07-21 01:22:46 +0000

    atualizações no projeto web

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """"<h1>DevOps WebApp Project</h1>
-    <p>Aplicação rodando com sucesso na EC2!</p>
-    <p>Upando Aplicação no GitHub</p>""""
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
