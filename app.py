1b0ef38b51d1b1521af87f16c830e3fcdaae91b6
Juan <jjuanleon.ti@gmail.com>
2026-07-21 01:05:58 +0000

    Alterações do Index.Js

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>DevOps Web Project</h1><p>Aplicação rodando com sucesso na EC2!</p>'
+    return """"<h1>DevOps WebApp Project</h1>
+    <p>Aplicação rodando com sucesso na EC2!</p>
+    <p>Upando Aplicação no GitHub</p>""""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
