296fed854e2f6e6de04ae692967634542856bbe4
EC2 Default User <ec2-user@ip-172-31-85-7.ec2.internal>
2026-08-11 21:33:34 +0000

    atualizando pacotes e versão do python 3.11

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 cd /home/ec2-user/devops-web-project
-
-pkill -f "python3 app.py" || true
-
-nohup python3 app.py > app.log 2>&1 &
+pkill -f "python3.11 app.py" || true
+nohup python3.11 app.py > app.log 2>&1 &
